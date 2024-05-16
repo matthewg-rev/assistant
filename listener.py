d30eed9e15fb04e14cc4e6f5ff7dcd112317f501
matthewg-rev <130177336+matthewg-rev@users.noreply.github.com>
2024-05-16 00:49:43 -0700

    chore: Update listener.py with speech recognition and streaming functionality

--- listener.py
+++ listener.py
@@ -55,3 +55,23 @@ RESPEAKER_INDEX = 1
 CHUNK = 1024
 
 p = pyaudio.PyAudio()
+
+stream = p.open(
+    rate=RESPEAKER_RATE,
+    format=p.get_format_from_width(RESPEAKER_WIDTH),
+    channels=RESPEAKER_CHANNELS,
+    input=True,
+    input_device_index=RESPEAKER_INDEX,
+    frames_per_buffer=CHUNK,
+)
+
+r = speech_recognition.Recognizer()
+
+while True:
+    data = stream.read(CHUNK)
+    try:
+        text = r.recognize_vosk(data)
+        print(text)
+    except Exception as e:
+        print(e)
+        sys.exit()
